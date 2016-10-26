967df40afadc589832d33e87ad51a58f0f7dcaba
fuwillie <fuwillie@us.ibm.com>
2016-10-26 09:44:10 -0700

    Video page

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCT6vVmVhNuT3cvWTQo4KFZfFf0tWs0Mbc';
 
 
-// Create a new component. This component should produce some html
-const App = () => {
-    return (
-    <div>
-        <SearchBar/>
-    </div>);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    };
 };
-// Take this component's generated HTML and put it on the page (in the DOM)
+
 ReactDom.render(<App />, document.querySelector('.container'));
